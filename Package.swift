e32631c37dd352c560233350c9876593299f6219
aafanasi <afanasev.anton@gmail.com>
2025-09-03 01:10:09 -0300

    [Release RC] 2.9.2-rc3

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloudMessengerTransport",
-            url: "https://github.com/MyPureCloud/genesys-messenger-transport-mobile-sdk/releases/download/v2.9.3-rc1/MessengerTransport.xcframework.zip",
-            checksum: "2b34600aaa9f66b15692da71463286b3642fecad64c561734e2b6df7d1eb4770"
+            url: "https://github.com/MyPureCloud/genesys-messenger-transport-mobile-sdk/releases/download/v2.9.3-rc3/MessengerTransport.xcframework.zip",
+            checksum: "0bcfbc773882602072e46bae84c668fe63767daf2f58ff5535cc90e30dbe556d"
         ),
         .testTarget(name: "PackageTests", dependencies: [
             "GenesysCloudMessengerTransport"
