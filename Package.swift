654789f475e442495af4e292c3370a2d2c6eda4d
Yarin Sason <yarin.sason@genesys.com>
2026-02-25 15:17:39 +0200

    [Release RC] 2.12.0-rc3

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloudMessengerTransport",
-            url: "https://github.com/MyPureCloud/genesys-messenger-transport-mobile-sdk/releases/download/v2.11.0-rc6/MessengerTransport.xcframework.zip",
-            checksum: "1e8c6a6dd2862ef996cee78f10caa97c62ea32bc158f1bc1d5c2f38de66e61d4"
+            url: "https://github.com/MyPureCloud/genesys-messenger-transport-mobile-sdk/releases/download/v2.12.0-rc3/MessengerTransport.xcframework.zip",
+            checksum: "659ecfc20c0aa2246114d335be93a8eecdaa98e355f3b7e19f8e53cb4f76ef0a"
         ),
         .testTarget(name: "PackageTests", dependencies: [
             "GenesysCloudMessengerTransport"
